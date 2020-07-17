069fccbb70415b042d51b6b8b0d3af397923b9fc
Dan Mosora <dan@stitchdata.com>
2020-07-17 19:10:23 +0000

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-amazon-ads-dsp',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the Amazon Advertising DSP v1.0 API',
       author='scott.coleman@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
